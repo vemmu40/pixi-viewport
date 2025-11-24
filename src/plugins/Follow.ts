ef138e20660fa074c4117ffd59f01075fd75b2bc
vemmu40 <vertti.vehkonen@gmail.com>
2025-11-24 15:57:57 +0200

    works

--- src/plugins/Follow.ts
+++ src/plugins/Follow.ts
@@ -38,7 +38,7 @@ const DEFAULT_FOLLOW_OPTIONS: Required<IFollowOptions> = {
     speed: 0,
     acceleration: null,
     radius: null,
-    offset: { x: 0, y: 0 }
+    offset: { x: 400, y: 0 }
 };
 
 /**
